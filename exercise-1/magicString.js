fbbdb54d7cabcd6649967809bdf1ef7728d4faa0
Gabriel <luquegabrielies@gmail.com>
2025-02-26 19:04:05 +0100

    exercises 1 2

--- exercise-1/magicString.js
+++ exercise-1/magicString.js
@@ -3,3 +3,8 @@ const userName = "randomUser";
 if(userName == "randomuser"){
     console.log(userName);
 }
+
+const camelCase = "camelCase";
+const PascalCase = "PascalCase";
+const snake_Case = "snake_case";
+const kebabcase = "kebab-case";
