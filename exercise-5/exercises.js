4b345b784e32a3c5dd3f218e7bdc4c5a578ff637
GabrielLu73 <luquegabrielies@gmail.com>
2025-02-27 10:02:21 +0100

    excercises complete

---
+++ exercise-5/exercises.js
@@ -0,0 +1,41 @@
+const myMap = new Map();
+
+const user = {};
+const user2 = {};
+
+function findAnimal(){};
+
+myMap.set("moroso", user);
+myMap.set("agarrado", user);
+myMap.set("generoso", user2);
+myMap.set("funcion", findAnimal());
+myMap.set("color","color")
+
+for (const [key, values] of myMap.entries()){
+    console.log(key, values);
+}
+
+for (const key of myMap.keys()){
+    const KEY_MOROSO = "moroso";
+    
+    if(myMap.has(KEY_MOROSO)){
+        console.log(key);
+        break;
+    }
+}
+
+const isKey = myMap.has("hola");
+console.log(isKey);
+
+const mapSize = myMap.size;
+console.log(mapSize);
+
+myMap.forEach((value, key) => {
+    console.log(key, value);
+});
+
+myMap.delete("agarrado");
+console.log(myMap);
+
+myMap.clear();
+console.log(myMap);
